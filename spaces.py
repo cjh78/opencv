9d28abdccfb446f07fa44f26adf8160449dab13b
connor jayden <connorhassan@hotmail.co.uk>
2023-11-08 15:20:51 +0000

    Uncommented out the spaces file and created split merge file

--- spaces.py
+++ spaces.py
@@ -6,19 +6,19 @@ img = cv.imread("Photos/boston.jpg")
 
 cv.imshow("Boston", img)
 
-# grey = cv.cvtColor(img, cv.COLOR_BAYER_BG2GRAY)
-# cv.imshow("Grey", grey)
+grey = cv.cvtColor(img, cv.COLOR_BAYER_BG2GRAY)
+cv.imshow("Grey", grey)
 
 
-# ## BGR to HSV (Hue Saturation Vision) ##
+## BGR to HSV (Hue Saturation Vision) ##
 
-# hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-# cv.imshow("HSV", hsv)
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow("HSV", hsv)
 
-# ### BGR TO L A B ###
+### BGR TO L A B ###
 
-# lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
-# cv.imshow("L-A-B ", lab)
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow("L-A-B ", lab)
 
 
 
---
+++ split_merge.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+
+
+
+img = cv.imread("Photos/boston.jpg")
+cv.imshow("Normal", img)
+
+
+
+
+
+cv.waitKey(0)
